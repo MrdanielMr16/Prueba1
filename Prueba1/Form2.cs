5e559dc04acb247565e1b77c425918348f3ceb65
Daniel Marin <Danielfmarin16@gmail.com>
2021-09-10 16:13:47 -0500

    Codigo de Form2

--- Prueba1/Form2.cs
+++ Prueba1/Form2.cs
@@ -16,5 +16,11 @@ namespace Prueba1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            this.Dispose();
+        }
     }
 }
