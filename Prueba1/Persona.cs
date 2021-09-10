30b6aa2e69e06f829eb468e166c714a204ddeb2b
Daniel Marin <Danielfmarin16@gmail.com>
2021-09-10 16:34:54 -0500

    Se crea clase y se crean variables de clase Persona

---
+++ Prueba1/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prueba1
+{
+    class Persona
+    {
+        String Cedula;
+        String Nombre;
+        int Edad;
+        int Fecha_Nacimiento;
+    }
+}
